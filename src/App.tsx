215fea4b4e1119ab2f35a070990caf1d31840c1f
Nikolas Santis <nikosantis@gmail.com>
2020-11-30 14:41:10 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,7 @@ const App: FC = () => {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <p>Hello World</p>
       </header>
     </div>
   );
