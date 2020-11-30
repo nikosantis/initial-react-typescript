11240cc706369bbb41d062064287ceb2e785b6cb
Nikolas Santis <nikosantis@gmail.com>
2020-11-30 14:48:33 -0300

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const App: FC = () => {
   return (
     <div className='App'>
       <header className='App-header'>
+        <h1>Bx React TypeScript Skeleton</h1>
         <p>Hello World</p>
       </header>
     </div>
